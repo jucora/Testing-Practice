03e0c68f12cb14be727523a2685991c5f1479646
jucora <artjulius@hotmail.com>
2020-06-03 23:20:31 -0500

    calculator object and test completed

--- functions.test.js
+++ functions.test.js
@@ -13,3 +13,28 @@ describe("reverseString", () => {
     expect(functions.reverseString(myString)).toBe("olleh");
   });
 });
+
+describe("calculator", () => {
+  const num1 = 10;
+  const num2 = 2;
+  describe("add", () => {
+    it("performs the addition of two values", () => {
+      expect(functions.calculator.add(num1, num2)).toBe(12);
+    });
+  });
+  describe("subtract", () => {
+    it("performs the subtraction of two values", () => {
+      expect(functions.calculator.subtract(num1, num2)).toBe(8);
+    });
+  });
+  describe("divide", () => {
+    it("performs the division of two values", () => {
+      expect(functions.calculator.divide(num1, num2)).toBe(5);
+    });
+  });
+  describe("multiply", () => {
+    it("performs the multiplication of two values", () => {
+      expect(functions.calculator.multiply(num1, num2)).toBe(20);
+    });
+  });
+});
