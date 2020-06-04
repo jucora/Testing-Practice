ed04d0dfa7665f294c29d8aae31d75ef0caeb773
jucora <artjulius@hotmail.com>
2020-06-03 21:38:15 -0500

    reverseString function test completed

--- functions.js
+++ functions.js
@@ -2,4 +2,12 @@ const capitalize = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-module.exports = capitalize;
+const reverseString = (string) => {
+  if (string.length === 0) {
+    return string;
+  } else {
+    return reverseString(string.substr(1)) + string[0];
+  }
+};
+
+module.exports = { capitalize, reverseString };
--- functions.test.js
+++ functions.test.js
@@ -1,8 +1,15 @@
-const capitalize = require("./functions");
+const functions = require("./functions");
 
 describe("capitalize", () => {
   it("should return a string with the first character capitalized", () => {
     const myString = "hello";
-    expect(capitalize(myString)).toBe("Hello");
+    expect(functions.capitalize(myString)).toBe("Hello");
+  });
+});
+
+describe("reverseString", () => {
+  it("should returns a string reversed.", () => {
+    const myString = "hello";
+    expect(functions.reverseString(myString)).toBe("olleh");
   });
 });
